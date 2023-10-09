6dbb8c856f84a66c281b8e46e325d409277a5931
DELORD Clément <cdelord@dawan.fr>
2023-10-09 16:02:29 +0200

    exemple avec html dans le fichier php

--- form.php
+++ form.php
@@ -1,10 +1,5 @@
-<?php
-
-
-echo "<ul>";
-foreach ($_POST as $index => $value) {
-    echo "<li>";
-    echo $index . " => " . $value;
-    echo "</li>";
-}
-echo "</ul>";
+<ul>
+<?php foreach ($_POST as $index => $value) { ?>
+<li><?=$index?> => <?=$value?></li>
+<?php } ?>
+</ul>
